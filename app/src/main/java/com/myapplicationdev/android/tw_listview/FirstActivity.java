aab8e31327903c3adf1aaaffb15b553302520c7c
15017573 <15017573@myrp.edu.sg>
2017-04-28 15:15:07 +0800

    Changed first activity for intent Added the module class Changed the module adapter class Fixed array adpater

--- app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
@@ -1,16 +1,20 @@
 package com.myapplicationdev.android.tw_listview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
 
 import java.util.ArrayList;
 
 public class FirstActivity extends AppCompatActivity {
 
     ListView lv;
-    ArrayList<String> al;
+    ArrayList<String> year;
     ArrayAdapter<String> aa;
 
     @Override
@@ -18,17 +22,30 @@ public class FirstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        al = new ArrayList<String>();
-        al.add("Year 1");
-        al.add("Year 2");
-        al.add("Year 3");
+        lv = (ListView)this.findViewById(R.id.lvYear);
 
-        aa = new ArrayAdapter<String>(this, R.layout.activity_main, al);
+        year = new ArrayList<String>();
+        year.add("Year 1");
+        year.add("Year 2");
+        year.add("Year 3");
+
+        aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, year);
+
         lv.setAdapter(aa);
 
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedYear = year.get(position).toString();
 
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
 
+                intent.putExtra("year", selectedYear);
+
+                startActivity(intent);
+            }
+        });
 
     }
 }
