821f6792f81bcab5e2459196c5db869e0eed18a6
15017573 <15017573@myrp.edu.sg>
2017-04-28 13:36:26 +0800

    Changed first activity for intent Added the module class Changed the module adapter class

--- app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
@@ -1,11 +1,17 @@
 package com.myapplicationdev.android.tw_listview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+
+import static com.myapplicationdev.android.tw_listview.R.id.tvYear;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -25,6 +31,25 @@ public class FirstActivity extends AppCompatActivity {
 
         aa = new ArrayAdapter<String>(this, R.layout.activity_main, al);
         lv.setAdapter(aa);
+
+//        Intent i = getIntent();
+//        String year = i.getStringExtra("year");
+//        tvYear.setText(year);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedModule = al.get(position);
+
+                Intent i = new Intent(FirstActivity.this, SecondActivity.class);
+                i.putExtra("modules", selectedModule);
+                startActivity(i);
+
+
+            }
+        });
+
 
 
 
--- app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
@@ -3,13 +3,20 @@ package com.myapplicationdev.android.tw_listview;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import static com.myapplicationdev.android.tw_listview.R.id.tvYear;
 
 public class SecondActivity extends AppCompatActivity {
 
     ListView lv;
     TextView tvYear;
+    ArrayList<Module> modules;
+    ArrayAdapter aa;
 
 
     @Override
@@ -23,6 +30,14 @@ public class SecondActivity extends AppCompatActivity {
         Intent i = getIntent();
         String year = i.getStringExtra("year");
         tvYear.setText(year);
+
+        modules = new ArrayList<Module>();
+        modules.add(new Module("C208", true));
+
+        aa = new ModuleAdapter(this, R.layout.row, modules);
+        lv.setAdapter(aa);
+
+
 
 
 
